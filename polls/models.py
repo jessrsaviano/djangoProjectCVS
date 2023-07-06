276834a78f399521d8ed7cd01ef433e8e06065a8
Jess <jrs5xrw@virginia.edu>
2023-07-06 12:01:04 -0400

    first html

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
+
+import datetime
+
+from django.contrib import admin
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+
+
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import  Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
